4be3b15a22f9f195d4bf2f000118877646ac5074
Claudine Chionh <info@claudinec.net>
2024-03-27 10:30:26 +1100

    set HLEDGER_UI_EDITOR

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
     fish_vi_key_bindings
 
-    # git editor
+    # set program-specific editors
     set -gx GIT_EDITOR gvim --nofork
+    set -gx HLEDGER_UI_EDITOR gvim
 
     test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
